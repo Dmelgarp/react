0a4afd3401ed88439ce52ea5082a94b8d45e67d0
Diego Melgar Parada <melgarparadadiego@gmail.com>
2026-07-24 16:51:04 -0400

    1.0.2

--- proyecto/src/components/primerComponente.jsx
+++ proyecto/src/components/primerComponente.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 
 export const PrimerComponente = () => {
+    let cursos = [
+        "Ingerieria Termonuclear Avanzada",
+        "Como tomar agua",
+        "Boxeo contra kanguros",
+    ]
+
+
     return (
         <>
             <h1>Primer Componente</h1>
-            <p>Cualquier cosa</p>
+            <p>Cursos disponibles</p>
+            <ul>
+                {
+                    cursos.map( (curso, index) => {
+                        return(
+                            <li key={index}>{curso}</li>
+                        )
+                    })
+                }
+            </ul>
+            <button onClick={ e => (cambiarCurso("Tomar alcohol"))}>
+                Cambiar curso
+            </button>
         </>
         
     )
--- proyecto/src/components/segundoComponente.jsx
+++ proyecto/src/components/segundoComponente.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 export const SegundoComponente = () => {
+    let nombre = "Diego";
+    let web = "www.yo.com";
+    let edad = 2;
+    
     return (
         <div>
             <h1>Segundo Componente</h1>
             <ul>
-                <li>Dato 1</li>
-                <li>Dato 2</li>
-                <li>Dato 3</li>
+                <li>Mi nombre es: {nombre}</li>
+                <li>Mi edad es: {edad}</li>
+                <li>Mi sitio web es: {web}</li>
             </ul>
         </div>
     )
