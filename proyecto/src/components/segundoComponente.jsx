a3adf4593cbcf4cb81100a6aeffc03a870b6b238
Diego Melgar Parada <melgarparadadiego@gmail.com>
2026-08-11 14:51:54 -0400

    app 1.0.7

--- proyecto/src/components/segundoComponente.jsx
+++ proyecto/src/components/segundoComponente.jsx
@@ -1,16 +1,48 @@
 "use client"
 
-import React, {useSatate} from "react"
+// Funciones externas
+import React, {useState} from "react"
 
+function CambiarCurso(curso, nuevoCurso){
+    const nuevosCursos = [...curso]
+    nuevosCursos[1] = nuevoCurso
+    return nuevosCursos
+}
+
+// Estructura principal
 export function SegundoComponente () {
     const nombre = "Diego Melgar Parada"
     const edad = 3
     const web = "www.odoo.com"
 
+    const [cursos, setCursos] = useState([
+        "Teoria de la ingenieria termonuclear",
+        "Como tomar agua",
+        "Investigacion de la materia",
+    ])
 
+// Resultado devuelto
     return(
         <>
             <h1>Segundo componente</h1>
+            <p> Mi nombre es : {nombre}</p>
+            <p> Mi edad es : {edad}</p>
+            <p> Mi pagina web es : {web}</p>
+            <hr/>
+            <h2>Cursos Disponibles</h2>
+            <ul>
+                {
+                    cursos.map((curso, index) => {
+                        return (
+                            <li  key={index}>{curso}</li>
+                        )
+                    })
+                }
+            </ul>
+            <input type="text"/>
+            <button type="button" onClick={ () => setCursos(CambiarCurso(cursos, "Como tomar alcohol"))}>
+                Cambiar curso
+            </button>
         </>
     )
 }
