545369e65742c374a58e9e7802a401a423608693
Diego Melgar Parada <melgarparadadiego@gmail.com>
2026-09-22 16:17:50 -0400

    app 1.0.15

--- proyecto/src/main.jsx
+++ proyecto/src/main.jsx
@@ -2,11 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Funciones from './reactES6/arrowFunctions';
 import ArrowFun from './reactES6/arrowFunctions2';
+import ArrowHead from './reactES6/arrowFunctions3';
 
 const myElement = (
     <>
-        <ArrowFun/>
-
+        <ArrowHead/>
     </>
 );
 
---
+++ proyecto/src/reactES6/variables.jsx
@@ -0,0 +1,25 @@
+<>
+    <p>hay 3 fromas de declarar una variable.
+        let
+        var
+        const
+
+        Las variables var tienen alcance global
+
+        Las variables let y const tienen alcance de bloques.
+        si declara una variable fuera de un bloque, tiene alcance global, 
+        pero si la declaras dentro de un for por ejemplo, solo prodras usarla 
+        dentro del for.
+
+        las variables const son una referencia constante a un valor.
+        por eso no puedes:
+            reasignar un valor
+            reasignar un array
+            reasignar un objeto
+        pero puedes:
+            cambiar los elementos de un array
+            cambiar las propiedades de un objeto
+        
+        usando useEstate tambien se pueden cambiar los valores de constantes.
+    </p>
+</>
